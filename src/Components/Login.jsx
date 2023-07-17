8f62f0666d06fd243a65a989e119c6f8432a9aaf
Kiran-7700 <121047254+Kiran-7700@users.noreply.github.com>
2023-07-17 13:53:11 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -51,10 +51,10 @@ function Login() {
             />
           </div>
           <div className="button_group" id="login_button">
-            <a onClick={submit}>Submit</a>
+            <a onClick={submit} href='#'>Submit</a>
           </div>
           <div className="footer">
-            <a >Forgot Password ?</a>
+            <a href='#'>Forgot Password ?</a>
             <Link to="/register" className='link'>SignUp</Link>
           </div>
         </div>
